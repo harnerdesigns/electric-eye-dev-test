30a679c9af6fcf49cfd298427e4ca17e3799af9a
Jack Harner <jack@harnerdesigns.com>
2021-05-09 17:29:03 -0600

    Error Handling for Out of Stock Variant Selection
    * Change ATC Button Label to "Sold Out" and Disables the button if
    an unavailable variant is selected.
    * Resets ATC Button Label to "Add To Cart" or "Buy Now" if an available
    variant is selected after an unavailable variant.

--- assets/ee_buy_now.js
+++ assets/ee_buy_now.js
@@ -65,37 +65,51 @@ function ee_variantSelectChanged(e) {
   var parentDiv = self.closest("div");
   var currentImg = parentDiv.querySelector(".product-card__image-wrapper img");
   var currentURL = parentDiv.querySelector("a");
+  var currentATCButton = parentDiv.querySelector(".ee-atc-btn");
+  var currentATCButtonLabel = currentATCButton.querySelector("span");
 
   var selectedOption = self.querySelector("option:checked");
   var selectedId = selectedOption.dataset.variant_id;
   var selectedPrice = selectedOption.dataset.variant_price;
   var selectedImage = selectedOption.dataset.variant_image_url;
   var selectedURL = selectedOption.dataset.variant_url;
+  var selectedAvailable = "true" === selectedOption.dataset.variant_available;
 
-  // Change Add To Cart Button Variant ID
-  parentDiv.querySelector(".ee-atc-btn").dataset.variant_id = selectedId;
-  parentDiv.querySelector(
-    ".price .price__regular dd .price-item"
-  ).innerHTML = selectedPrice;
+  if (selectedAvailable) {
+      // Resetting ATC Button if a Out Of Stock Option was selected first.
+    currentATCButtonLabel.innerHTML = currentATCButtonLabel.dataset.og_atc_text;
+    currentATCButton.removeAttribute('disabled');
 
-  // Change Image
-  if (selectedImage != "" && !selectedImage.includes("no-image")) {
-    //Clear Out Old Image
-    currentImg.removeAttribute("srcset");
-    currentImg.removeAttribute("sizes");
-    currentImg.removeAttribute("src");
-    currentImg.dataset.srcset = "";
-    currentImg.dataset.widths = "";
-    currentImg.classList.remove("lazyloaded");
 
-    // Set New Image and Trigger Lazy Load
-    currentImg.dataset.src = selectedImage;
-    currentImg.classList.add("lazyload");
-  }
+    // Change Add To Cart Button Variant ID
+    currentATCButton.dataset.variant_id = selectedId;
+    parentDiv.querySelector(
+      ".price .price__regular dd .price-item"
+    ).innerHTML = selectedPrice;
 
-  // Change Link URL
-  if (selectedURL != "") {
-    currentURL.setAttribute("href", selectedURL);
+    // Change Image
+    if (selectedImage != "" && !selectedImage.includes("no-image")) {
+      //Clear Out Old Image
+      currentImg.removeAttribute("srcset");
+      currentImg.removeAttribute("sizes");
+      currentImg.removeAttribute("src");
+      currentImg.dataset.srcset = "";
+      currentImg.dataset.widths = "";
+      currentImg.classList.remove("lazyloaded");
+
+      // Set New Image and Trigger Lazy Load
+      currentImg.dataset.src = selectedImage;
+      currentImg.classList.add("lazyload");
+    }
+
+    // Change Link URL
+    if (selectedURL != "") {
+      currentURL.setAttribute("href", selectedURL);
+    }
+  } else {
+    // User Somehow Selected an "Out of Stock" Variant
+    currentATCButtonLabel.innerHTML = "Sold Out";
+    currentATCButton.setAttribute('disabled', "true")
   }
 }
 
